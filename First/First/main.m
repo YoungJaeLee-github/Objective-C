127a027c84e013f30f366c5c133a044db77f7067
Youngjae Lee <77099686+YoungJaeLee-github@users.noreply.github.com>
2021-01-29 23:07:34 +0900

    Add files via upload

---
+++ First/First/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  First
+//
+//  Created by 이영재 on 2021/01/29.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
