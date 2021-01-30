2189cba93a2d50e77c897b375c4987b4acdad37d
Youngjae Lee <77099686+YoungJaeLee-github@users.noreply.github.com>
2021-01-30 19:36:05 +0900

    Update main.m

--- First/First/main.m
+++ First/First/main.m
@@ -9,7 +9,7 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int wheels = 4;
+        int wheels = 4; // int long float double - primitive type(원시(기본) 자료형)
         int seats = 2;
         NSLog(@"Wheels: %i, Seats: %i", wheels, seats);
     }
